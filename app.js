0513a49763fa9b3629bd57f09a9ee0f58f99b437
Bruno Cuzzi <brunocuzzi@MacBook-Pro-di-Bruno.local>
2018-06-18 23:28:22 +0200

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://bc58bc58:dW0xYxNzTA2J@ds261040.mlab.com:61040/local_library';
+var mongoDB = process.env.MONGODB_URI || 'mongodb://bc58bc58:dW0xYxNzTA2J@ds261040.mlab.com:61040/local_library';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
